f6eaafb724b24ffb9ffb44b8d6865897521d61b8
quang.td95 <quang.td95@gmail.com>
2016-12-25 10:42:49 +0700

    Init Repo.

---
+++ app/src/main/java/td/quang/mvpshowcustomers/Views/Activities/MainActivity.java
@@ -0,0 +1,15 @@
+package td.quang.mvpshowcustomers.Views.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import td.quang.mvpshowcustomers.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
